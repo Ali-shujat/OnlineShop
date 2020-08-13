b9f740e7c7ea8f1f424e3779045bf320170e54e6
Shujat Ali <shuja4you@hotmail.com>
2020-08-13 13:00:34 +0100

    Basket UI design , fixing some bugs

---
+++ MyShop.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,49 @@
+﻿using MyShop.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        IBasketService basketService;
+
+        public BasketController(IBasketService service)
+        {
+            this.basketService = service;
+        }
+
+        // GET: Basket
+        public ActionResult Index()
+        {
+            var model = basketService.GetBasketItems(this.HttpContext);
+            return View(model);
+        }
+
+        //Add To Basket
+        public ActionResult AddProduct(string Id)
+        {
+            basketService.AddToBasket(this.HttpContext, Id);
+            return RedirectToAction("Index");
+        }
+        //Remove items from Basket
+        public ActionResult RemoveProduct(string Id)
+        {
+            basketService.RemoveFromBasket(this.HttpContext, Id);
+            return RedirectToAction("Index");
+        }
+
+        //partial view of the Basket 
+        public PartialViewResult BasketSummary()
+        {
+            var basketSummary = basketService.GetBasketSummary(this.HttpContext);
+
+            return PartialView(basketSummary);
+        }
+
+
+    }
+}
